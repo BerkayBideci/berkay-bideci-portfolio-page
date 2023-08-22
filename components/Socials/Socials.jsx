70d959d1814d52b6141d278a2915d911c9e525c1
Berkay Bideci <berkaybideci@gmail.com>
2023-08-22 19:56:05 +0300

    Feat: add header, socials, style

--- components/Socials/Socials.jsx
+++ components/Socials/Socials.jsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+import { RiGithubFill, RiLinkedinFill } from "react-icons/ri"
+
 const Socials = () => {
-    return <div>Socials</div>;
+    return <div className="flex items-center gap-x-5 text-lg xl:text-2xl">
+        <Link href="https://github.com/BerkayBideci" target='_blank' className="hover:text-accent transition-all duration-300">
+            <RiGithubFill />
+        </Link>
+        <Link href="https://www.linkedin.com/in/berkay-bideci-8902921b1/" target='_blank' className="hover:text-accent transition-all duration-300">
+            <RiLinkedinFill />
+        </Link>
+    </div>;
 };
 
 export default Socials;
