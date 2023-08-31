a627c04f875706db928df288fcc5da3d78b2cd8b
Berkay Bideci <berkaybideci@gmail.com>
2023-08-31 19:49:17 +0300

    Feat: add style and writings to about page, add cv

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -4,10 +4,14 @@ import {
     SiJavascript,
     SiReact,
     SiNextdotjs,
+    SiTailwindcss,
+    SiBootstrap,
     SiFramer,
     SiRedux,
     SiFirebase
 } from "react-icons/si";
+import { FaLightbulb, FaSearch, FaUsers, FaTasks, FaSyncAlt } from 'react-icons/fa';
+import { BsDownload } from "react-icons/bs"
 import Avatar from "../../components/Avatar/Avatar"
 import Circles from "../../components/Circles/Circles"
 import { useState } from "react";
@@ -17,24 +21,86 @@ import CountUp from "react-countup";
 
 const aboutData = [
     {
+        title: 'Resume',
+        info: [
+            {
+                title: "",
+                cv: {}
+            }
+        ],
+    },
+    {
         title: 'skills',
         info: [
             {
                 title: 'Technical Skills',
-                icons: [
-                    <SiHtml5 />,
-                    <SiCss3 />,
-                    <SiJavascript />,
-                    <SiReact />,
-                    <SiNextdotjs />,
-                    <SiFramer />,
-                    <SiRedux />,
-                    <SiFirebase />
+                technologies: [
+                    {
+                        name: "HTML",
+                        icon: <SiHtml5 />,
+                    },
+                    {
+                        name: "CSS",
+                        icon: <SiCss3 />,
+                    },
+                    {
+                        name: "JavaScript",
+                        icon: <SiJavascript />,
+                    },
+                    {
+                        name: "React.js",
+                        icon: <SiReact />,
+                    },
+                    {
+                        name: "Next.js",
+                        icon: <SiNextdotjs />,
+                    },
+                    {
+                        name: "Tailwind",
+                        icon: <SiTailwindcss />,
+                    },
+                    {
+                        name: "Bootstrap",
+                        icon: <SiBootstrap />,
+                    },
+                    {
+                        name: "Framer",
+                        icon: <SiFramer />,
+                    },
+                    {
+                        name: "Redux",
+                        icon: <SiRedux />,
+                    },
+                    {
+                        name: "Firebase",
+                        icon: <SiFirebase />
+                    },
                 ],
             },
             {
                 title: 'Soft Skills',
-                icons: []
+                notions: [
+                    {
+                        name: "Creativity",
+                        icon: <FaLightbulb />,
+                    },
+                    {
+                        name: "Detail-oriented",
+                        icon: <FaSearch />,
+                    },
+                    {
+                        name: "Collaboration",
+                        icon: <FaUsers />,
+                    },
+                    {
+                        name: "Multi-tasking",
+                        icon: <FaTasks />,
+                    },
+                    {
+                        name: "Adaptability",
+                        icon: <FaSyncAlt />,
+                    }
+                ]
             }
             // {
             //     title: 'UI/UX Design',
@@ -76,22 +142,13 @@ const aboutData = [
         title: 'credentials',
         info: [
             {
-                title: 'Web Development - ABC University, LA, CA',
-                stage: '2011',
+                title: 'Front-End Web Development Bootcamp - Re:Coded',
+                stage: '2023',
             },
             {
-                title: 'Computer Science Diploma - AV Technical Institute',
-                stage: '2009',
+                title: 'Russian Language and Literature - Ankara University',
+                stage: '2021',
             },
-            {
-                title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-                stage: '2006',
-            },
-        ],
-    },
-    {
-        title: 'Resume',
-        info: [
         ],
     },
 ];
@@ -132,7 +189,7 @@ const About = () => {
                         initial="hidden"
                         animate="show"
                         exit="hidden"
-                        className="hidden md:flex md:max-w-xl mx-auto xl:mx-0 mb-8">
+                        className="hidden md:flex md:max-w-xl mx-auto xl:mx-0 mb-4 xl:mb-8">
                         <div className="flex justify-center items-center flex-1 gap-x-6">
                             <div className="flex-auto w-40 border-y border-l p-3 border-black/60 dark:border-white/10 rounded-s-xl">
                                 <div className="text-2xl xl:text-4xl font-extrabold dark:text-accent text-accentLight mb-2 text-start">
@@ -177,39 +234,74 @@ const About = () => {
                     className="flex flex-col w-full xl:max-w-[48%] h-[530px]">
                     <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
                         {aboutData.map((item, itemIndex) => {
-                            return (
-                                <div
-                                    key={itemIndex}
-                                    className={`${index === itemIndex && "dark:text-accent text-accentLight after:w-[100%] after:bg-accentLight dark:after:bg-accent after:transition-all after:duration-300"} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
-                                    onClick={() => setIndex(itemIndex)}>
-                                    {item.title}
-                                </div>
-                            )
+                            if (item.title !== "Resume") {
+                                return (
+                                    <div
+                                        key={itemIndex}
+                                        className={`${index === itemIndex && "dark:text-accent text-accentLight after:w-[100%] after:bg-accentLight dark:after:bg-accent after:transition-all after:duration-300"} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0 hidden xl:flex`}
+                                        onClick={() => setIndex(itemIndex)}>
+                                        {item.title}
+                                    </div>
+                                )
+                            } else {
+                                return (
+                                    <div
+                                        key={itemIndex}
+                                        className={`${index === itemIndex && "dark:text-accent text-accentLight after:w-[100%] after:bg-accentLight dark:after:bg-accent after:transition-all after:duration-300"} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                                        onClick={() => setIndex(itemIndex)}>
+                                        {item.title}
+                                    </div>
+                                )
+                            }
                         })}
                     </div>
                     <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
                         {aboutData[index].info.map((item, itemIndex) => {
-                            return (
-                                <div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-black/60 dark:text-white/60">
-                                    <div
-                                        className="font-light mb-2 md:mb-0">
-                                        {item.title}
+                            if (item.title === "") {
+                                return (
+                                    <a key={itemIndex} href="Berkay_Bideci_CV.pdf" download="Berkay_Bideci_CV.pdf" target="_blank" className="btn rounded-full border border-black/60 dark:border-white/50 max-w-[250px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accentLight dark:hover:border-accent group">
+                                        <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
+                                            Download Resume
+                                        </span>
+                                        <BsDownload className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
+                                    </a>
+                                )
+                            } else {
+                                return (
+                                    <div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center justify-center text-black/60 dark:text-white/60">
+                                        <div
+                                            className="font-light mb-2 md:mb-0 whitespace-nowrap">
+                                            {item.title}
+                                        </div>
+                                        <div className="hidden md:flex">-</div>
+                                        <div>{item.stage}</div>
+                                        <div className="flex flex-wrap gap-4 max-w-xs">
+                                            {item.technologies?.map((tech, itemIndex) => {
+                                                return (
+                                                    <div
+                                                        key={itemIndex}
+                                                        className="text-black dark:text-white flex flex-col justify-center items-center w-16">
+                                                        <div className="text-2xl mb-1 hover:text-accentLight dark:hover:text-accent">{tech.icon}</div>
+                                                        <div className="text-sm">{tech.name}</div>
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                        <div className="flex flex-wrap gap-y-4 gap-x-2 max-w-md">
+                                            {item.notions?.map((notion, itemIndex) => {
+                                                return (
+                                                    <div
+                                                        key={itemIndex}
+                                                        className="text-black dark:text-white flex flex-col justify-center items-center w-[7rem]">
+                                                        <div className="text-2xl mb-1 hover:text-accentLight dark:hover:text-accent">{notion.icon}</div>
+                                                        <div className="text-sm">{notion.name}</div>
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
                                     </div>
-                                    <div className="hidden md:flex">-</div>
-                                    <div>{item.stage}</div>
-                                    <div className="flex gap-x-4">
-                                        {item.icons?.map((icon, itemIndex) => {
-                                            return (
-                                                <div
-                                                    key={itemIndex}
-                                                    className="text-2xl text-black dark:text-white">
-                                                    {icon}
-                                                </div>
-                                            )
-                                        })}
-                                    </div>
-                                </div>
-                            )
+                                )
+                            }
                         })}
                     </div>
                 </motion.div>
